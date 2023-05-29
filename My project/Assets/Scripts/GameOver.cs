77abf84527d806d0c203c08945b5b8187d29a799
Kuan Min <chenkuanmin1204@gmail.com>
2023-05-29 15:22:25 +0800

    Github test

--- My project/Assets/Scripts/GameOver.cs
+++ My project/Assets/Scripts/GameOver.cs
@@ -4,6 +4,7 @@ using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
+    // Test
     public TextMeshProUGUI roundsSurvived;
     // when this Panel is first enabled, it will display rounds survived.
     private void OnEnable()
