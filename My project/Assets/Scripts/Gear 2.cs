7c0938a845e742bc829499fe77d85b4b7b0af8df
Guo Yuhao <guoyuhao4@gmail.com>
2023-07-14 14:02:53 +0800

    Fixing Main

--- My project/Assets/Scripts/Gear 2.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class Gear : MonoBehaviour
-{
-    public abstract void EffectOnTurret(Turret turret);
-    public abstract void EffectOnEnemy(Enemy enemy);
-}
--- My project/Assets/Scripts/IGearAbility 2.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IGearAbility
-{
-    public abstract void EffectOnTurret();
-    public abstract void EffectOnEnemy();
-}
